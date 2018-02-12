c5f03c0725a288fbe2a405c84269ddf361189a49
Jacob Sayles <jsayles@gmail.com>
2018-02-12 15:36:53 -0800

    moved to examples.py

--- src/buzz.py
+++
@@ -1,8 +0,0 @@
-import board
-import bitbangio
-import adafruit_drv2605
-i2c = bitbangio.I2C(board.SCL, board.SDA)
-drv = adafruit_drv2605.DRV2605(i2c)
-drv.use_ERM()
-drv.set_waveform(14)
-drv.play()
