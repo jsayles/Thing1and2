cccf8000f1b4ccad3429abc98c8af27eac91a3cc
Jacob Sayles <jsayles@gmail.com>
2018-02-25 11:20:12 -0800

    moved pin0 to be power led

--- src/settings.py
+++ src/settings.py
@@ -13,17 +13,10 @@ THING2_PORT = 8082
 thing1_addr = socket.getaddrinfo(THING1_IP, THING1_PORT)[0][-1]
 thing2_addr = socket.getaddrinfo(THING2_IP, THING2_PORT)[0][-1]
 
-PIN_RED = 16
-PIN_BLUE = 12
-PIN_GREEN = 13
-PIN_BUTTON = 2
-PIN_VIBE = 14
-PIN_VIBE_LED = 0
-
 # Setup our hardware
-red_led = machine.Pin(PIN_RED, machine.Pin.OUT, value=1)
-blue_led = machine.Pin(PIN_BLUE, machine.Pin.OUT, value=1)
-green_led = machine.Pin(PIN_GREEN, machine.Pin.OUT, value=1)
-button = machine.Pin(PIN_BUTTON, machine.Pin.IN, machine.Pin.PULL_UP)
-vibe = machine.Pin(PIN_VIBE, machine.Pin.OUT, value=0)
-vibe_led = machine.Pin(PIN_VIBE_LED, machine.Pin.OUT, value=1)
+power_led = machine.Pin(0, machine.Pin.OUT, value=0)
+red_led = machine.Pin(16, machine.Pin.OUT, value=1)
+blue_led = machine.Pin(12, machine.Pin.OUT, value=1)
+green_led = machine.Pin(13, machine.Pin.OUT, value=1)
+button = machine.Pin(2, machine.Pin.IN, machine.Pin.PULL_UP)
+vibe = machine.Pin(14, machine.Pin.OUT, value=0)
