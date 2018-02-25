bdcc2489360cb436f51117eaeb3e6e9f5c15e18d
Jacob Sayles <jsayles@gmail.com>
2018-02-24 18:35:17 -0800

    add led status and some vibe

--- src/thing1.py
+++ src/thing1.py
@@ -2,9 +2,15 @@ import settings
 import machine
 import network
 import socket
+import time
+import vibe
+import led
 
 from utils import send_value, watch_for_value
 
+
+# Start out with our LED red
+led.red()
 
 # Access Point Interface (creating a wifi network)
 print("Turning on the Access Point (%s)" % settings.WIFI_ESSID)
@@ -15,15 +21,19 @@ ap.active(True)
 ap.config(essid=settings.WIFI_ESSID, password=settings.WIFI_PASSWD, authmode=3, channel=11, hidden=1)
 ap.ifconfig((settings.THING1_IP, '255.255.255.0', settings.THING1_IP, '8.8.8.8'))
 
-
 # Hook up our button interupt to send the value over to Thing2
 def button_handler(pin):
     send_value(settings.thing2_addr, pin.value())
 trigger = machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING
 settings.button.irq(trigger=trigger, handler=button_handler)
 
-
 # Listen for values comming from Thing2
 def thing2_watcher(msg):
     print("Message from Thing2: '%s'" % msg)
+    vibe.pulse(.5)
 watch_for_value(settings.thing1_addr, thing2_watcher)
+
+# Turn the LED green to indicate we're ready to go
+led.green()
+time.sleep(1)
+led.off()
--- src/thing2.py
+++ src/thing2.py
@@ -2,9 +2,14 @@ import settings
 import machine
 import network
 import socket
+import time
+import vibe
+import led
 
 from utils import send_value, watch_for_value
 
+# Start out with our LED red
+led.red()
 
 # Access Point Interface (creating a wifi network)
 print("Connecting to Access Point (%s)" % settings.WIFI_ESSID)
@@ -21,4 +26,10 @@ settings.button.irq(trigger=trigger, handler=button_handler)
 
 def thing1_watcher(msg):
     print("Message from Thing1: '%s'" % msg)
+    vibe.pulse(.5)
 watch_for_value(settings.thing2_addr, thing1_watcher)
+
+# Turn the LED green to indicate we're ready to go
+led.green()
+time.sleep(1)
+led.off()
