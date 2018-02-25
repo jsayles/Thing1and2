2f78cf216e9962e3db8160715297c1396a55bbd1
Jacob Sayles <jsayles@gmail.com>
2018-02-25 11:19:32 -0800

    Now takes full serial port

--- firmware/flash.sh
+++ firmware/flash.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-esptool.py --port /dev/tty.SLAB_USBtoUART  erase_flash
-esptool.py --port /dev/tty.SLAB_USBtoUART --baud 460800 write_flash --flash_size=detect 0 adafruit-circuitpython-feather_huzzah-2.2.3.bin
+if [ -z "$1" ]; then
+  echo "Available Ports: "
+  ls /dev/tty.SLAB_USBtoUART*
+  exit 1
+fi
+PORT=$1
+echo "Using $PORT"
+esptool.py --port $PORT  erase_flash
+esptool.py --port $PORT --baud 460800 write_flash --flash_size=detect 0 adafruit-circuitpython-feather_huzzah-2.2.3.bin
