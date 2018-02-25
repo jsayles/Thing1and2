66b307974a1aa63b8fedeb52f5f694eb7baf5c41
Jacob Sayles <jsayles@gmail.com>
2018-02-24 19:50:32 -0800

    brute force it

--- install.sh
+++ install.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 source bin/activate
+
+echo "Flashing the firmware..."
+cd firmware
+./flash.sh
+cd -
+
+#for i in *.py;
+#do
+#   echo "Uploading $i...";
+#   ampy -p /dev/tty.SLAB_USBtoUART put $i;
+#done
+
+echo "Uploading the source files..."
 cd src
-for i in *.py;
-do
-   echo "Uploading $i...";
-   ampy -p /dev/tty.SLAB_USBtoUART put $i;
-done
+ampy -p /dev/tty.SLAB_USBtoUART put settings.py
+ampy -p /dev/tty.SLAB_USBtoUART put utils.py
+ampy -p /dev/tty.SLAB_USBtoUART put led.py
+ampy -p /dev/tty.SLAB_USBtoUART put vibe.py
+ampy -p /dev/tty.SLAB_USBtoUART put thingnet.py
+ampy -p /dev/tty.SLAB_USBtoUART put thing1.py
+ampy -p /dev/tty.SLAB_USBtoUART put thing2.py
+cd -
+
