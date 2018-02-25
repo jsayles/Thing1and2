62d7416d0a0f60d964f91b1166e9a38c1cc778ce
Jacob Sayles <jsayles@gmail.com>
2018-02-24 18:42:33 -0800

    added basic vibe to button

--- src/main.py
+++ src/main.py
@@ -11,3 +11,12 @@ import led
 # Hardware Test
 led.cycle()
 vibe.pulse()
+
+# Hook up our button to vibe when pressed
+def button_handler(pin):
+    if pin.value() == 0:
+        vibe.on()
+    else:
+        vibe.off()
+trigger = machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING
+settings.button.irq(trigger=trigger, handler=button_handler)
