de2f75458ea113fe402b78c309dbbc360e4fd651
Jacob Sayles <jsayles@gmail.com>
2018-03-11 10:28:43 -0700

    removed tie to specific version number

--- firmware/flash.sh
+++ firmware/flash.sh
@@ -7,4 +7,4 @@ fi
 PORT=$1
 echo "Using $PORT"
 esptool.py --port $PORT  erase_flash
-esptool.py --port $PORT --baud 460800 write_flash --flash_size=detect 0 adafruit-circuitpython-feather_huzzah-2.2.3.bin
+esptool.py --port $PORT --baud 460800 write_flash --flash_size=detect 0 adafruit-circuitpython-feather_huzzah-*.bin
