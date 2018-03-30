aa8ef13d3807dec7ad7e13153f25e819e3faf487
Jacob Sayles <jsayles@gmail.com>
2018-03-30 08:27:43 -0700

    working on status lights

--- src/core.py
+++ src/core.py
@@ -1,7 +1,7 @@
 import time
 import network
 import socket
-
+import errno
 
 THING1 = 1
 THING2 = 2
@@ -124,6 +124,15 @@ class RGB_LED:
             time.sleep(seconds)
         self.off()
 
+    def cycle_on(self, seconds=1):
+        self.off()
+        self.red_on()
+        time.sleep(seconds)
+        self.green_on()
+        time.sleep(seconds)
+        self.blue_on()
+        time.sleep(seconds)
+
 
 class Vibe:
     # PWM Settings for adjustable vibe strength
@@ -172,36 +181,115 @@ class Thingnet:
         split_ip[3] = str(THING1)
         self.thing1_ip = ".".join(split_ip)
         self.thing1_port = 8080 + THING1
-        self.thing1_addr = (self.thing1_ip, self.thing1_port)
+        self.thing1_address = socket.getaddrinfo(self.thing1_ip, self.thing1_port)[0][-1]
 
         # Calculate the ip for Thing2
         split_ip[3] = str(THING2)
         self.thing2_ip = ".".join(split_ip)
         self.thing2_port = 8080 + THING2
-        self.thing2_addr = (self.thing2_ip, self.thing2_port)
+        self.thing2_address = socket.getaddrinfo(self.thing2_ip, self.thing2_port)[0][-1]
 
+        # Network Variables
+        self.network_interface = None
+        self.incoming_socket = None
+        self.remote_address = None
+        self.local_address = None
+
+    ''' Create an access point for THINGNET. '''
     def create_thingnet(self):
         print("Turning on THINGNET (SSID:  %s)" % self.ssid)
-
-        # Turn off the other interface
-        network.WLAN(network.STA_IF).active(False)
-
-        # Create an access point for THINGNET
         ap = network.WLAN(network.AP_IF)
         ap.active(True)
         ap.config(essid=self.ssid, password=self.passwd, authmode=3, channel=11, hidden=1)
         ap.ifconfig((self.thing1_ip, '255.255.255.0', self.thing1_ip, '8.8.8.8'))
         return ap
 
+    ''' Join the THINGNET network. '''
     def join_thingnet(self):
         print("Connecting to THINGNET (SSID: %s)" % self.ssid)
-
-        # Turn off the other interface
-        network.WLAN(network.AP_IF).active(False)
-
-        # Join the THINGNET network
         wlan = network.WLAN(network.STA_IF)
         wlan.active(True)
         wlan.connect(self.ssid, self.passwd)
         wlan.ifconfig((self.thing2_ip, '255.255.255.0', self.thing1_ip, '8.8.8.8'))
         return wlan
+
+    ''' Turn off all network interfaces. '''
+    def stop(self):
+        network.WLAN(network.STA_IF).active(False)
+        network.WLAN(network.AP_IF).active(False)
+        self.network_interface = None
+
+    ''' Start up our network. '''
+    def start(self):
+        # No good start comes before a stop
+        self.stop()
+
+        if self.thing_id == THING1:
+            self.local_address = self.thing1_address
+            self.remote_address = self.thing2_address
+            self.network_interface = self.create_thingnet()
+        else:
+            self.network_interface = self.join_thingnet()
+            self.local_address = self.thing2_address
+            self.remote_address = self.thing1_address
+
+    def wait_until_connected(self):
+        if not self.network_interface:
+            raise Exception("Network not started.  Call start()")
+        while not self.network_interface.isconnected():
+            time.sleep(0.6)
+        return True
+
+    def open_incoming_socket(self):
+        sock = socket.socket()
+        # sock.settimeout(timeout)
+        # sock.setblocking(False)
+        # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        sock.setblocking(True)
+        sock.bind(self.local_address)
+        sock.listen(1)
+
+    def close_incoming_socket(self):
+        if self.incoming_socket:
+            self.incoming_socket.close()
+
+    def receive_value(self):
+        if not self.incoming_socket:
+            raise Exception("No incoming socket established!")
+        value = -1
+        client = None
+        try:
+            client, client_address = self.incoming_socket.accept()
+            print("Client connected from %s:%d" % client_address)
+            # client_file = client.makefile('rwb', 0)
+            # line = client_file.readline()
+            line = client.readline()
+            if line[:6] == b'VALUE:':
+                value = int(line[6:])
+            client.send("OK")
+            print("   Recieved Value: %d" % value)
+        # except Exception as e:
+        #     # if e.args[0] == errno.ETIMEDOUT:
+        #     #     print("  Timeout!")
+        #     print("  Problem receiving: %s" % str(e))
+        finally:
+            if client:
+                client.close()
+        return value
+
+    def send_value(self, value):
+        msg = "VALUE:%d" % value
+        print("Sending '%s' to %s:%d" % (msg, self.remote_address[0], self.remote_address[1]))
+        outgoing_socket = socket.socket()
+        try:
+            outgoing_socket.connect(self.remote_address)
+            outgoing_socket.send(bytes(msg+"\r\n\r\n", 'utf8'))
+            return_msg = str(outgoing_socket.recv(64), 'utf8')
+            if return_msg == "OK":
+                print("  Message Received!")
+            else:
+                print("  Error: '%s'" % return_msg)
+        except Exception as e:
+            print("  Problem sending: %s" % str(e))
+        finally:
+            outgoing_socket.close()
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import time
 import socket
 from machine import Pin, Timer
 
@@ -5,10 +6,7 @@ from core import PRESS, RELEASE, Thingnet
 from utils import send_value, watch_for_value
 
 
-# Network Variables
-network_interface = None
-remote_address = None
-local_address = None
+thingnet = None
 
 # LEDs to indicate local and remote activity
 local_led = None
@@ -22,36 +20,39 @@ remote_led = None
 
 ''' Called when the button is pressed. '''
 def local_handler(pin):
+    if not thingnet:
+        # Do nothing!
+        return
     value = pin.value()
     if value == PRESS:
         local_led.on()
     else:
         local_led.off()
-    send_value(remote_address, value)
+    thingnet.send_value(remote_address, value)
 
 
-''' Called when a button press action comes in from the network.'''
-def remote_handler(value):
-    if value == PRESS:
-        remote_led.on()
-        vibe.on()
-    else:
-        remote_led.off()
-        vibe.off()
+# ''' Called when a button press action comes in from the network.'''
+# def remote_handler(value):
+#     if value == PRESS:
+#         remote_led.on()
+#         vibe.on()
+#     else:
+#         remote_led.off()
+#         vibe.off()
 
 
-''' Called periodicly to check the network status. '''
-def timer_handler(timer):
-    print("Checking THINGNET Connectivity")
-    if not network_interface:
-        print("  Interface not initialized!")
-        return
-    if network_interface.isconnected():
-        print("  Connected!")
-        main_led.red_off()
-    else:
-        print("  Not Connected!")
-        main_led.red_on()
+# ''' Called periodicly to check the network status. '''
+# def timer_handler(timer):
+#     print("Checking THINGNET Connectivity")
+#     if not network_interface:
+#         print("  Interface not initialized!")
+#         return
+#     if network_interface.isconnected():
+#         print("  Connected!")
+#         main_led.red_off()
+#     else:
+#         print("  Not Connected!")
+#         main_led.red_on()
 
 
 #####################################################################
@@ -59,32 +60,57 @@ def timer_handler(timer):
 #####################################################################
 
 # Hardware Test
-power_led.off()
-main_led.cycle(seconds=.2)
+main_led.cycle_on(.2)
 vibe.pulse(seconds=.2)
-power_led.on()
-red_led.on()
 
-# Fire up our Network
-thingnet = Thingnet(THING_ID, WIFI_SSID, WIFI_PASS, IP_RANGE)
+# Determine the local and remote LEDs
 if I_AM_THING1:
-    network_interface = thingnet.create_thingnet()
-    local_address = thingnet.thing1_addr
-    remote_address = thingnet.thing2_addr
     local_led = blue_led
     remote_led = green_led
 else:
-    network_interface = thingnet.join_thingnet()
-    local_address = thingnet.thing2_addr
-    remote_address = thingnet.thing1_addr
     local_led = green_led
     remote_led = blue_led
-
-# Start a timer to watch our network
-Timer(-1).init(period=1000, callback=timer_handler, mode=Timer.PERIODIC)
 
 # Hook up our button to the local handler
 button.irq(handler=local_handler, trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING)
 
+# Fire up our Network
+thingnet = Thingnet(THING_ID, WIFI_SSID, WIFI_PASS, IP_RANGE)
+
+# Main Loop
+while True:
+    main_led.on()
+    thingnet.start()
+
+    main_led.red()
+    thingnet.wait_until_connected()
+
+    main_led.orange()
+    thingnet.open_incoming_socket()
+
+    main_led.off()
+    try:
+        while True:
+            # Blocking call to wait for a value
+            value = thingnet.receive_value()
+            if value == PRESS:
+                remote_led.on()
+                vibe.on()
+            else:
+                remote_led.off()
+                vibe.off()
+    except Exception as e:
+        print("Lost connection: %s" % str(e))
+    finally:
+        thingnet.close_incoming_socket()
+
+    # A small delay before we try again
+    time.sleep(1.0)
+
+
+# Start a timer to watch our network
+# Timer(-1).init(period=1000, callback=timer_handler, mode=Timer.PERIODIC)
+
+
 # Listen for values coming in over the network
-watch_for_value(local_address, remote_handler)
+# watch_for_value(local_address, remote_handler)
